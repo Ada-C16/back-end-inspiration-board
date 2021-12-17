aeb3a457dcbcf28149008ccc110c20497ff47881
karishma <karishmacarterjohnson@gmail.com>
2021-12-17 15:52:06 -0800

    set up routes and register

--- app/board_routes.py
+++ app/board_routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
+from flask import Blueprint, request, make_response, jsonify
+import requests
+import os
 
-# example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+@boards_bp.route("", methods=["GET"])
+def handle_boards():
+    pass
---
+++ app/card_routes.py
@@ -0,0 +1,11 @@
+from app import db
+from app.models.board import Board
+from flask import Blueprint, request, make_response, jsonify
+import requests
+import os
+
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+@cards_bp.route("", methods=["GET"])
+def handle_cards():
+    pass
