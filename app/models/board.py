6273968afa055627df156a793f5ff40992ef9c92
Jesse <jesseannepope@gmail.com>
2021-12-17 15:32:07 -0800

    created Board and Card model

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,10 @@
 from app import db
+from flask import current_app
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+    cards = db.relationship("Card", backref="board", lazy=True)
+
+
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+from flask import current_app
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
