d510657bb14adc283ffec62611b3d28550b5f407
Faith <fkauwe@gmail.com>
2021-12-19 16:51:09 -0800

    Board and Card classes created

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+# board needs id, title, author
+# building the database table with columns, data type, column names
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    author= db.Column(db.String)
