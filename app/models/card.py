41856420c4f58c6295544f58a206c754f330d032
Jesse <jesseannepope@gmail.com>
2021-12-20 16:26:47 -0800

    added autoincrement and default value

--- app/models/card.py
+++ app/models/card.py
@@ -2,7 +2,7 @@ from app import db
 from flask import current_app
 
 class Card(db.Model):
-    card_id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer) #potentially give default value
+    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
