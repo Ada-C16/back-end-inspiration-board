1ac3a27e6678db1af9c70cc60a99977d119968d1
Veronica Osei <veronicaosei273@gmail.com>
2021-12-21 17:36:22 -0600

    Helper functions for cards and board created

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,11 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+
+    def board_dict(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "owner": self.owner
+        }
--- app/models/card.py
+++ app/models/card.py
@@ -1,6 +1,14 @@
 from app import db
 
 class Card(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+
+
+    def card_dict(self):
+        return{
+            "card_id": self.card_id,
+            "message":self.message,
+            "likes_count": self.likes_count
+        }
