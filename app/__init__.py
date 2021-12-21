1d67efe1b8e560b87227d29f8229aed293350fb3
Asli Athman <asliathman@gmail.com>
2021-12-21 13:38:43 -0500

    added a to_dict() helper function in the card model, and a 'get' method to the cards route

--- app/__init__.py
+++ app/__init__.py
@@ -27,8 +27,8 @@ def create_app():
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
-    from .routes import board_bp
-    app.register_blueprint(board_bp)
+    from .routes import boards_bp
+    app.register_blueprint(boards_bp)
 
     from .routes import cards_bp
     app.register_blueprint(cards_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
-board_bp = Blueprint ("board", __name__, url_prefix=("/board"))
+board_bp = Blueprint ("boards", __name__, url_prefix=("/boards"))
 cards_bp = Blueprint ("cards", __name__, url_prefix=("/cards"))
+
+#CARDS
+#read - GET
+@cards_bp.route("", methods=["GET"])
+def get_card():
+    cards = Card.query.all()
+
+    response = []
+    for card in cards:
+        response.append(card.to_dict())
+
+    return jsonify(response), 200
+
+#create - POST
+
+#delete - DELETE
+
+
+
+#BOARDS
+
+#read - GET
+
+#create - POST
+
+#delete - DELETE
