f411e10781eb558531dac1431930d307adc1e4ca
brew doctor <>
2021-12-21 22:37:31 +0400

    completed GET for boards

--- app/routes.py
+++ app/routes.py
@@ -5,21 +5,30 @@ from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
-@boards_bp.route("", methods = ["POST"])
+@boards_bp.route("", methods = ["POST", "GET"])
 def handle_boards():
-    request_body = request.get_json()
-    if "title" not in request_body.keys() or "owner" not in request_body.keys():
-            return make_response({"details": "Invalid data"}, 400) 
-    else:
-        new_board = Board(title=request_body['title'],
-                    owner=request_body['owner'])
-        db.session.add(new_board)
-        db.session.commit()
-        return make_response({"board": {"id": new_board.board_id, 
-        "title":new_board.title,'owner':new_board.owner}},201)
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body.keys() or "owner" not in request_body.keys():
+                return make_response({"details": "Invalid data"}, 400) 
+        else:
+            new_board = Board(title=request_body['title'],
+                        owner=request_body['owner'])
+            db.session.add(new_board)
+            db.session.commit()
+            return make_response({"board": {"id": new_board.board_id, 
+            "title":new_board.title,'owner':new_board.owner}},201)
+    elif request.method == "GET":
+        boards = Board.query.all()
+        boards_response = []
 
-
-
+        for board in boards:
+            boards_response.append({
+                "id": board.board_id,
+                "title": board.title,
+                "owner": board.owner
+            }) 
+        return jsonify(boards_response)
 
 @boards_bp.route("/<board_id>", methods = ["GET"])
 def handle_board(board_id):
