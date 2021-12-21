706a08adb6cdf7c5472fa5051b52e2d32c2df26e
cecily-916 <cecily.marroquin@gmail.com>
2021-12-21 11:24:17 -0500

    pseudo-coded all anticipated endpoints and enhancement ideas

--- app/routes/boards_routes.py
+++ app/routes/boards_routes.py
@@ -7,3 +7,27 @@ from app import db
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
 
+# POST /boards Creates a new board.
+# params: title (string), name (string)
+# returns a dictionary with board data. 
+@boards_bp.route("", methods=["POST"])
+# def write function here....
+
+# GET /boards Gets a list of all boards.
+# returns a dictionary of boards data.
+# return empty array if no boards have been created.
+@boards_bp.route("", methods=["GET"])
+# def 
+
+
+# GET /boards/<board_id> Gets data for specific board.
+# returns a dictionary of the board's data.
+@boards_bp.route("/<board_id>", methods=["GET"])
+
+# GET /boards/<board_id>/cards Gets all cards assigned to a specific board.
+# returns a dictionary of cards data for the board.
+@boards_bp.route("/<board_id>/cards", methods=["GET"])
+# def
+
+
+# Enhancement ideas - DELETE, PUT/PATCH board info
