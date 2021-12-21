a876d6a0f1e1e10ae3211844fc50b35ee7c44039
brew doctor <>
2021-12-21 22:31:23 +0400

    completed GET boards for specific id and POST boards

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,42 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
+boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
+@boards_bp.route("", methods = ["POST"])
+def handle_boards():
+    request_body = request.get_json()
+    if "title" not in request_body.keys() or "owner" not in request_body.keys():
+            return make_response({"details": "Invalid data"}, 400) 
+    else:
+        new_board = Board(title=request_body['title'],
+                    owner=request_body['owner'])
+        db.session.add(new_board)
+        db.session.commit()
+        return make_response({"board": {"id": new_board.board_id, 
+        "title":new_board.title,'owner':new_board.owner}},201)
+
+
+
+
+@boards_bp.route("/<board_id>", methods = ["GET"])
+def handle_board(board_id):
+    try:
+        board = Board.query.get(board_id)
+    except:
+        return {"details": "Invalid data"}, 400 # For when you enter /asdjsaiod instead of a num
+
+    if board is None:
+        return {"message": f" Board {board_id} not found"}, 404 # For when you enter /5 but there is no board_id of 5 in db
+    
+    # If valid, then return response abt the specifoc board
+    return {
+        "board": {
+            "id": board.board_id,
+            "title": board.title,
+            "owner": board.owner
+        }
+    }
+
