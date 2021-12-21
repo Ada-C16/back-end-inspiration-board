b2effe6213351dfd90007d96e16b941d120368a0
Faith <fkauwe@gmail.com>
2021-12-21 15:43:00 -0800

    endpoints for Card(delete one card) and get all cards all functioning in Postman

--- app/routes.py
+++ app/routes.py
@@ -35,3 +35,30 @@ def get_all_cards():
     for card in cards:    
         cards_response.append(card.convert_to_dict())
     return jsonify(cards_response), 200
+
+
+@cards_bp.route("/<card_id>", methods=["GET", "PUT", "DELETE", "PATCH"])
+def CRUD_one_card(card_id):
+    card = Card.query.get(card_id) #either get Card back or None, card here is an object
+    if card is None:
+        return make_response({"message": f"Card {card_id} was not found"}, 404)
+
+    # PATCH will change just one part of the record, not the whole record
+    # not required but adding a patch for total_inventory on 11.9.21
+
+    # if request.method == "PATCH":
+    #     form_data = request.get_json()
+    # if "increase_likes" in form_data:
+    #     card.increase_likes = form_data["increase_likes"]
+    # db.session.commit()
+    # return make_response({"video": {"id": video.id,
+    #                 "title": video.title,
+    #                 "release_date": video.release_date,
+    #                 "total_inventory": video.total_inventory}}, 200)
+
+    if request.method == "DELETE":
+    # query db for specific card object by the card id
+        if Card.query.filter_by(id=card_id):
+            db.session.delete(card)
+            db.session.commit()
+        return make_response({'message': f"Card {card.id} was deleted"}, 200)
