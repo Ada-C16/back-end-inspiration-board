c831fe3bf77e89daeb8656c2bee3fcc1f03f3e4b
syblades <86990570+syblades@users.noreply.github.com>
2021-12-21 23:19:24 -0800

    added CREATE logic for board and card

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,64 @@ from app.models.card import Card
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
+
+# CREATE
+# A new board
+@boards_bp.route("", methods=['POST'])
+def create_board():
+    request_body = request.getjson()
+    if "name" not in request_body or "title" not in request_body:
+        return jsonify("Not Found"), 404
+
+    # name represents "Owners name" in the form on the frontend
+    new_board = Board(name=request_body["name"], title=request_body["title"])
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return jsonify(f"Board: {new_board.name} successfully created."), 201
+
+
+#  Create a new card within a board
+@boards_bp.route("/<board_id>/cards", methods=['POST'])
+def create_card(board_id):
+    board = Board.query.get(board_id)
+    if board is None:
+        return jsonify("Not Found"),404
+
+    request_body = request.getjson()
+    card_ids = request_body["card_ids"]
+
+    for card_id in card_ids:
+        card = Card.query.get(card_id)
+        card.board_id = int(board_id)
+
+    new_cards = []
+    for card in board.cards:
+        new_cards.append(card.card_id)
+    # 'new cards' should return all newly created cards within a specific board  
+    response_body = {
+        "id": board.board_id,
+        "card_ids": new_cards,  
+    }
+
+    db.session.commit()
+
+    return jsonify(response_body), 200
+
+
+
+
+
+# READ
+# All cards within a board
+# All boards (board names) listed on Inspiration Board
+
+# UPDATE
+# Can update a board by adding new cards
+# Can update cards by adding 'likes'
+
+# DELETE
+# Can delete cards in a board
+# Can delete a single board
+# Can delete all cards and boards (? do we want to include this functionality?)
