e746fea9dfdea73ddcbda59e80b45096048e6138
Veronica Osei <veronicaosei273@gmail.com>
2021-12-22 07:19:26 -0600

    CRUD for cards

--- card.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class Card(db.Model):
-    card_id = db.Column(db.Integer, primary_key= True)
-    title = db.Column(db.String)
-    boards = db.relationship("Board", backref="Card", lazy = True)
