55312d9d62e7bd578da3427097bff002673367f5
syblades <86990570+syblades@users.noreply.github.com>
2021-12-22 12:21:19 -0800

    added notes about model relationships

--- app/models/board.py
+++ app/models/board.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import backref
 from app import db
 
+# Parent class
+# One board can have many cards
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
     cards = db.relationship("Card", backref="cards", lazy = True)
+    # ^^ cards = db.relationship("Card", backref="Board", lazy = True)
 
     def board_dict(self):
         return{
