145c3e90d0f1cc8e05227d8b610f75e4c2bb9224
kaitlyngore <kaitlyngore@gmail.com>
2021-12-22 16:27:04 -0500

    edit on test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,14 @@ from app.models.board import Board
 from app.models.card import Card
 from flask.signals import request_finished
 
-CARD_MESSAGE = "testing"
+BOARD_ID = 1
+TITLE = "Test Board"
+OWNER = "Test Group"
+
+CARD_ID = 1
+MESSAGE = "Test message"
+LIKES_COUNT = 3
+
 
 @pytest.fixture
 def app():
