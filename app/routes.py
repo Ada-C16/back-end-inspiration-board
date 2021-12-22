45bc0f5bad89542428841ffa988d95cc07509fbc
syblades <86990570+syblades@users.noreply.github.com>
2021-12-21 22:31:20 -0800

    imported Board and Card models

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
+from app.models.card import Card
 
 # example_bp = Blueprint('example_bp', __name__)
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
