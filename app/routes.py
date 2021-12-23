c9af7162acb8668ba0fc5d7ef4df676845ca484a
Veronica Osei <veronicaosei273@gmail.com>
2021-12-23 11:45:32 -0600

    "updated deleted and board"

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 
 # each HTTP method should have it's own function to follow the single responsbility principle
-@cards_bp.route("", methods=["GET","POST"])
+@cards_bp.route("", methods=["GET"])
 @cards_bp.route("/<board>/card")
 def retrieve_cards():
     if request.method == 'GET':
@@ -19,23 +19,6 @@ def retrieve_cards():
 
         return jsonify(cards_response), 200
 
-    # elif request.method == "POST":
-    #     #create card
-    #     request_body = request.get_json()
-    #     if "message" not in request_body or "likes_count" not in request_body:
-    #         return jsonify ({
-    #             "error meesage": "Invalid data"
-    #         }), 400
-    #     new_card = Card(
-    #         message = request_body["message"],
-    #         likes_count = request_body["likes_count"]
-    #     )
-
-        db.session.add(new_card)
-        db.session.commit()
-
-        response_body ={"card": new_card.card_dict()}
-        return jsonify(response_body),201
 
 @cards_bp.route("/<card_id>", methods= ["GET", "PUT","DELETE"])
 def retrieve_get_card(card_id):
@@ -61,11 +44,13 @@ def retrieve_get_card(card_id):
 def delete_board(board_id):
     board = Board.query.get(board_id) 
     # cards = #Card.query.get(board.cards) does this return card object or id(OBJECT IS BETTER)
+    # cascading delete 
 
     # for card in cards:
     for card in board.cards:
         db.session.delete(card)
-    db.session.delete(board_id)
+        #db.session.commit()
+    db.session.delete(board)
     db.session.commit()
 
 
