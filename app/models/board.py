dace9a5487effe7587e3ace1bb2e8cdaa8711cab
Megan <megan.hagar@gmail.com>
2021-12-23 11:24:22 -0800

    Set up models and relationships

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+from app.models.card import Card
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), nullable=False)
+    cards = db.relationship("Card", backref="board", lazy=True)
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
+    value = db.Column(db.String, nullable=False)
+    num_likes = db.Column(db.Integer, default=0)
+    
