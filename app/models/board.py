b95ffd340c11b202e9a2e2e6a5fbe826ca2d0da1
Victoria Duke <toriduke@alumni.stanford.edu>
2021-12-23 11:10:44 -0800

    establishes model relationship

--- app/models/board.py
+++ app/models/board.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Board(db.Model):
-    __tablename__ = "Board"
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+    card = db.relationship("Card", back_populates="board")
