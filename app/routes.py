6778f1d2315d83a9659f09725b7d27a762e7d0ba
Veronica Osei <veronicaosei273@gmail.com>
2021-12-23 14:50:49 -0600

    code

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,12 @@
 from flask import Blueprint, request, jsonify
 from app import db
+from app.models import board
 from app.models.card import Card
 from app.models.board import Board
 
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
-
-# each HTTP method should have it's own function to follow the single responsbility principle
-@cards_bp.route("", methods=["GET"])
-@cards_bp.route("/<board>/card")
-def retrieve_cards():
-    if request.method == 'GET':
-        cards = Card.query.all()
-        cards_response = []
-        for card in cards:
-            cards_response.append(card.card_dict())
-
-        return jsonify(cards_response), 200
 
 
 # CREATE
@@ -41,7 +30,7 @@ def create_board():
 @boards_bp.route("/<board_id>/cards", methods=['POST'])
 #front-end needs a click event to provide API call to backend with board id
 def create_card(board_id):
-    board = Board.query.get(board_id)
+    # board = Board.query.get(board_id)
     request_body = request.getjson()
 
     if "title" not in request_body or "message" not in request_body:
@@ -66,6 +55,14 @@ def retrieve_cards(board_id):
     cards_response = [card.card_dict() for card in board.cards]
 
     return jsonify(cards_response), 200
+
+# each HTTP method should have it's own function to follow the single responsbility principle
+@boards_bp.route("", methods=["GET"])
+def retrieve_boards():
+    boards = Board.query.all()
+    boards_response = [board.board_dict() for board in boards]
+    
+    return jsonify(boards_response), 200
 
 
 # All boards (board names) listed on Inspiration Board
@@ -115,34 +112,3 @@ def delete_card(card_id):
 
 
 
-# added back
-@cards_bp.route("/<card_id>", methods= ["GET", "PUT","DELETE"])
-def retrieve_get_card(card_id):
-    card = Card.query.get(card_id)
-    if "card" is None: 
-        return jsonify(None), 404
-    elif request.method == "GET":
-        pass
-    elif request.method == "PUT":
-        pass
-
-    elif request.method == "DELETE":
-        db.session.delete(card)
-        db.session.commit()
-
-        return {
-            "message": (f"Card {card_id} has been deleted")
-        }
-
-
-#Delete created and needs to be modified.    
-@boards_bp.route("/<board_id>", methods=["DELETE"])
-def delete_board(board_id):
-    board = Board.query.get(board_id) 
-    # cards = #Card.query.get(board.cards) does this return card object or id(OBJECT IS BETTER)
-
-    # for card in cards:
-    for card in board.cards:
-        db.session.delete(card)
-    db.session.delete(board_id)
-    db.session.commit()
