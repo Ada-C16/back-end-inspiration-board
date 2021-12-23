c381fd09cbffcee92175d6f26e932ff61e185802
Karina <infernormal@gmail.com>
2021-12-23 10:14:01 -0800

    finished setup

--- app/models/board.py
+++ app/models/board.py
@@ -1,3 +1,8 @@
 from app import db
 
 
+class Board(db.Model):
+    __tablename__ = "Board"
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
