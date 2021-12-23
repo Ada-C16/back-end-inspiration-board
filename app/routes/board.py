83ee094607ae27b4c573641442c8d4e6e15947cd
Lia Gaetano <lgaetano19@gmail.com>
2021-12-23 13:58:14 -0800

    creates empty routes module

---
+++ app/routes/board.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, jsonify, make_response
+from app import db
+
+# example_bp = Blueprint('example_bp', __name__)
+
+# GET /boards
+
+# POST /boards
+
+# GET /boards/<board_id>/cards
+
+# POST /boards/<board_id>/cards
