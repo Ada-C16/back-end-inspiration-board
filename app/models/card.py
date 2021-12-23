9dd115ac73c93cb66dade57c4fa4ec2e6db7d463
Faith <fkauwe@gmail.com>
2021-12-23 10:19:08 -0800

    Board routes written and testing successful in postman

--- app/models/card.py
+++ app/models/card.py
@@ -5,6 +5,10 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     increase_likes = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), primary_key=True, nullable=False)
+    # this relationship establishes a new attribute for each Card object, which allows it to access the Board object
+    # through Card.board and a second new attribute, Board.cards (plural bc each Board has multiple Cards)
+    board =db.relationship('Board', backref='cards')
 # this helper function takes a Card object and returns it as a JSON object/ dict
     def convert_to_dict(self):
         return { "id": self.id,
