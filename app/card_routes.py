da4f4a13ae36f654554f34075ccd4ce3670e66bb
karishma <karishmacarterjohnson@gmail.com>
2021-12-29 10:59:07 -0800

    revert to last working v

--- app/card_routes.py
+++ app/card_routes.py
@@ -1,8 +1,8 @@
 from app import db
 from app.models.card import Card
-from flask import Blueprint, request, jsonify
-from app.models.board import Board
-
+from flask import Blueprint, request, make_response, jsonify
+import requests
+import os
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
@@ -18,18 +18,14 @@ def handle_cards():
         })
     return jsonify(card_info), 200
 
-@cards_bp.route("/<boardid>", methods=["POST"])
-def post_card(boardid):
+@cards_bp.route("", methods=["POST"])
+def post_card():
     request_body = request.get_json()
     try: 
         card = Card(message = request_body["message"])
     except:
         return jsonify("unsuccessful post"), 400
-    board = Board.query.get(boardid)
-    if board is None:
-        return jsonify(""), 404
     db.session.add(card)
-    board.cards.append(card)
     db.session.commit()
     return jsonify("successful post"), 201
 
