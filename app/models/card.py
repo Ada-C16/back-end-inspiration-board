8523710ee88737e5815bd80c116bf28eac28165a
Faith <fkauwe@gmail.com>
2021-12-30 21:50:28 -0800

    created patch to increase likes. removed extra primary key fr board_id in Card table

--- app/models/card.py
+++ app/models/card.py
@@ -4,8 +4,8 @@ from app import db
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    increase_likes = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), primary_key=True, nullable=False)
+    likes = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)
     # this relationship establishes a new attribute for each Card object, which allows it to access the Board object
     # through Card.board and a second new attribute, Board.cards (plural bc each Board has multiple Cards)
     board =db.relationship('Board', backref='cards')
@@ -13,5 +13,5 @@ class Card(db.Model):
     def convert_to_dict(self):
         return { "id": self.id,
                 "message": self.message,
-                "increase_likes": self.increase_likes}
-                                        
+                "likes": self.likes}
+                                    
