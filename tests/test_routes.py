20fadc88b4442719716ae18990d3f84c081b6786
Sandra Caballero <spcaballero.2@gmail.com>
2022-01-02 14:28:44 -0800

    added first few tests

---
+++ tests/test_routes.py
@@ -0,0 +1,51 @@
+from app.models.card import Card
+
+def test_get_cards_no_saved_cards(client):
+    # Act
+    response = client.get("/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_cards_one_saved_cards(client, one_card):
+    # Act
+    response = client.get("/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body == [
+        {
+            "id": 1,
+            "message":"Keep doing YOU, boo",
+            "likes_count": 0
+        }
+    ]
+
+def test_get_card(client, one_card):
+    # Act
+    response = client.get("/cards/1")
+    response_body = response.get_json()
+    
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+            "id": 1,
+            "message":"Keep doing YOU, boo",
+            "likes_count": 0
+        }
+    ]
+
+def test_card_not_found(client, one_card):
+    # Act
+    response = client.get("/cards/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == None
