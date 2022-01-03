e5e4df9d5f4f89f84f0e2a58766906ebca5ab9c5
Makhabat <maksatbekova.m@gmail.com>
2022-01-03 11:33:38 -0800

    card model was created

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,18 @@
 from app import db
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+    message = db.Column(db.String, nullable=False)
+    like_count = db.Column(db.Integer, nullable=False)
+    # board = db.relationship("Rental", back_populates="video", lazy=True)
+
+
+    def to_dict(self):
+        return {
+            "id": self.id, 
+            "board_id": self.board_id,
+            "message": self.message,
+            "like_count": self.like_count
+        }
