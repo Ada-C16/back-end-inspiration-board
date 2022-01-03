4518e9e135ff5f810b82e5651e4d9f80a58483f9
Lux Barker <lux.barker13@gmail.com>
2022-01-03 15:21:23 -0800

    create and get cards, and add deleted column for cards

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,6 @@ boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
 # TODO routes/methods:
-# GET /boards/<board_id>/cards
-# POST /boards/<board_id>/cards
 # DELETE /cards/<card_id>
 # PUT /cards/card_id
 # OPTIONAL: DELETE /boards/<board_id>
@@ -23,28 +21,60 @@ def create_board():
     )
     db.session.add(new_board)
     db.session.commit()
-    response_body = {
+    resp = {
         "title": new_board.title,
         "owner": new_board.owner,
         "board_id": new_board.board_id
     }
-    return jsonify(response_body), 201
+    return jsonify(resp), 201
 
 
 @boards_bp.route("", methods=["GET"])
 def read_all_boards():
     boards = Board.query.all()
     
-    response_body = []
+    resp = []
 
     for board in boards:
-        response_body.append({
+        resp.append({
             "title": board.title,
             "owner": board.owner,
             "board_id": board.board_id
         })
 
-    return jsonify(response_body), 200
+    return jsonify(resp), 200
 
+@boards_bp.route("/<board_id>/cards", methods=["POST"])
+def create_card(board_id):
+    # optional enhancement: send slack message whenever a card is created
+    req = request.get_json()
+
+    new_card = Card(
+        message=req["message"],
+        board_id=board_id
+    )
+
+    db.session.add(new_card)
+    db.session.commit()
     
+    resp = {
+        "message": new_card.message,
+        "card_id": new_card.card_id,
+        "likes_count": new_card.likes_count
+    }
 
+    return jsonify(resp), 201
+
+
+@boards_bp.route("/<board_id>/cards", methods=["GET"])
+def read_all_cards_for_board(board_id):
+    board = Board.query.get_or_404(board_id)
+    resp = []
+    for card in board.cards:
+        if not card.deleted:
+            resp.append({
+                "card_id": card.card_id,
+                "message": card.message,
+                "likes_count": card.likes_count
+            })
+    return jsonify(resp), 200
