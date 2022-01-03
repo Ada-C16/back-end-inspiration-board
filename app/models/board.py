f3ca94fb71558b7e4b824f5a3b950f6a143487ac
Jessica <jessica.w.roberts@gmail.com>
2022-01-03 11:37:16 -0800

    Add foreign key reference to board

--- app/models/board.py
+++ app/models/board.py
@@ -4,4 +4,5 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    card = db.relationship("Card", back_populates="board", lazy=True)
 
