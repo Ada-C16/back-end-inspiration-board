261e50c5fc5cbf6af9b0e25c715b70615caf209b
Khandice <s.khandice@gmail.com>
2022-01-03 12:05:43 -0800

    Adds Board and Card models, registers those blueprints to the app
    Co-authored-by: Kristin <Kbhlee2121@users.noreply.github.com>

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,8 @@
 from app import db
+
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(40))
+    owner = db.Column(db.String(40))
+    cards = db.relationship("Card", back_populates="board")
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    likes = db.Column(db.Integer, default=None, nullable=True)
+    message = db.Column(db.String(180))
+    board = db.relationship("Board", back_populates="cards")
