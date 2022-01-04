9001c0e619f88a12b7be1b6d35ea077e56fdfdf1
sarahstandish <z.sisyphusrocks@gmail.com>
2022-01-03 17:50:12 -0800

    make card plural

--- app/models/card.py
+++ app/models/card.py
@@ -6,4 +6,4 @@ class Card(db.Model):
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
-    board = db.relationship("Board", back_populates="card")
+    board = db.relationship("Board", back_populates="cards")
