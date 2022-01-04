0c7e002ba2e23bf37205499d4d0733233f7d5ebe
Jessica <jessica.w.roberts@gmail.com>
2022-01-03 21:33:44 -0800

    Update foreign key column reference

--- app/models/card.py
+++ app/models/card.py
@@ -3,7 +3,7 @@ from app import db
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
     message = db.Column(db.String, nullable=False)
     like_count = db.Column(db.Integer, nullable=False)
     board = db.relationship("Board", back_populates="card",lazy=True)
