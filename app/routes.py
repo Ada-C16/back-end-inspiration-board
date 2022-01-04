e8b93136592bb3297fc8c80508f6c80c289dfb77
Mariah Rosman <mrosman7@gmail.com>
2022-01-03 17:01:50 -0800

    fixes bugs in board post request

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ from app.models.board import Board
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint('board_bp', __name__, url_prefix="/boards")
 
-@board_bp.route("", methods=["GET"])
-def get_all_boards():
+@board_bp.route("", methods=["GET", "POST"])
+def handle_boards():
     if request.method == "GET":
-        boards = Board.query.all()
+        boards = Board.query.all()  # list of board objects
         boards_response = []
         for board in boards:
             boards_response.append({
@@ -18,6 +18,7 @@ def get_all_boards():
             })
 
         return make_response(jsonify(boards_response), 200)
+
     elif request.method == "POST":
         request_body = request.get_json()
         new_board = Board(title = request_body["title"],owner = request_body["owner"])
@@ -25,7 +26,7 @@ def get_all_boards():
         db.session.add(new_board)
         db.session.commit()
 
-        return make_response(jsonify({
+        return make_response(jsonify({       #make method in Board Model for this
                 "board_id": new_board.board_id,
                 "title": new_board.title,
                 "owner": new_board.owner
