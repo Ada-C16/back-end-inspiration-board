04a3f3e549c32cf257f5feb2395ec5c08b7629c8
Zandra Nguyen <zandra.eng@gmail.com>
2022-01-04 17:49:06 -0800

    added log for missing owner and title in create board

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,7 @@ boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
 def create_board():
     request_body = request.get_json()
     if "owner" not in request_body or "title" not in request_body:
+        print("request failed validation. Missing 'owner' or 'title'.")
         return jsonify("Not Found"), 404
 
     new_board = Board(owner=request_body["owner"], title=request_body["title"])
@@ -22,12 +23,13 @@ def create_board():
     db.session.add(new_board)
     db.session.commit()
 
+    # need to return the ID because we already have the title.
     return jsonify(f"{new_board.title} successfully created."), 201
 
 
 # Create a new card
 @cards_bp.route("/<board_id>/cards", methods=['POST'])
-#front-end needs a click event to provide API call to backend with board id
+# front-end needs a click event to provide API call to backend with board id
 def create_card(board_id):
     board = Board.query.get(board_id)
     if board is None:
@@ -38,13 +40,13 @@ def create_card(board_id):
     if "message" not in request_body:
         return jsonify("Not Found"), 404
 
-    new_card = Card(board_id = board.id, message=request_body["message"], likes_count=0)
+    new_card = Card(board_id=board.id,
+                    message=request_body["message"], likes_count=0)
 
     db.session.add(new_card)
     db.session.commit()
 
     return jsonify(f"Card number {new_card.card_id} successfully created."), 201
-
 
 
 # READ
@@ -54,7 +56,7 @@ def retrieve_cards(board_id):
     board = Board.query.get(board_id)
     if board is None:
         return jsonify("Not Found"), 404
-        
+
     cards_response = [card.card_dict() for card in board.cards]
 
     return jsonify(cards_response), 200
@@ -64,7 +66,7 @@ def retrieve_cards(board_id):
 def retrieve_boards():
     boards = Board.query.all()
     boards_response = [board.board_dict() for board in boards]
-    
+
     return jsonify(boards_response), 200
 
 
@@ -87,7 +89,7 @@ def update_card(card_id):
 # Can delete a single board
 @boards_bp.route("/<board_id>", methods=["DELETE"])
 def delete_board(board_id):
-    board = Board.query.get(board_id) 
+    board = Board.query.get(board_id)
 
     for card in board.cards:
         db.session.delete(card)
@@ -100,12 +102,9 @@ def delete_board(board_id):
 # Can delete cards in a board
 @cards_bp.route("/<card_id>", methods=["DELETE"])
 def delete_card(card_id):
-    card = Card.query.get(card_id) 
+    card = Card.query.get(card_id)
 
     db.session.delete(card)
     db.session.commit()
 
     return jsonify(f"Card {card.card_id} successfully deleted.")
-
-
-
