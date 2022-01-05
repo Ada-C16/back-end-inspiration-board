0557d4e65531786c29807905a7470a7130341dd3
Asli Athman <asliathman@gmail.com>
2022-01-05 12:59:39 -0500

    made board_id non-nullable in the card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,7 +4,7 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
 
     def to_dict(self):
         card = {
